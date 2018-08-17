5724d1e1b9f656b1c9eb321666516728b8bdc6d0
hasee <hasee@dmm>
2018-08-17 21:03:00 +0800

    第一次提交 只有一个大体架子 有待完善

---
+++ src/main/java/com/dmm/Dayrik816workApplication.java
@@ -0,0 +1,12 @@
+package com.dmm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dayrik816workApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dayrik816workApplication.class, args);
+	}
+}
---
+++ src/main/java/com/dmm/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.dmm;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Dayrik816workApplication.class);
+	}
+
+}
